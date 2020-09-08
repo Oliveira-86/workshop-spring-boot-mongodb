da89d4753a34dfc63d837e312089a98d5fee1484
Oliveia-86 <egberto.duarte.14@gmail.com>
2020-09-08 17:28:29 -0300

    Obtendo um post por id

---
+++ src/main/java/com/oliveira/workshopmongo/service/PostService.java
@@ -0,0 +1,24 @@
+package com.oliveira.workshopmongo.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.oliveira.workshopmongo.domain.Post;
+import com.oliveira.workshopmongo.repository.PostRepository;
+import com.oliveira.workshopmongo.service.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repository;
+
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+}
