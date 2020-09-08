4c037cb3aa4c137e96c798da88d399c759f102b4
Oliveia-86 <egberto.duarte.14@gmail.com>
2020-09-08 18:01:08 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/oliveira/workshopmongo/config/Instantiation.java
+++ src/main/java/com/oliveira/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.oliveira.workshopmongo.domain.Post;
 import com.oliveira.workshopmongo.domain.User;
 import com.oliveira.workshopmongo.dto.AuthorDTO;
+import com.oliveira.workshopmongo.dto.CommentDTO;
 import com.oliveira.workshopmongo.repository.PostRepository;
 import com.oliveira.workshopmongo.repository.UserRepository;
 
@@ -23,6 +24,8 @@ public class Instantiation implements CommandLineRunner {
 	@Autowired
 	private PostRepository postRepository;
 	
+	
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
@@ -32,6 +35,7 @@ public class Instantiation implements CommandLineRunner {
 		userRepository.deleteAll();
 		postRepository.deleteAll();
 		
+		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
@@ -40,6 +44,13 @@ public class Instantiation implements CommandLineRunner {
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/0/2018"), "Bom dia", "Acordei feliz hoje", new AuthorDTO(maria));
+		
+		CommentDTO comment1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO comment2 = new CommentDTO("Aproveite!", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO comment3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("24/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(comment1, comment2));
+		post2.getComments().addAll(Arrays.asList(comment3));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
